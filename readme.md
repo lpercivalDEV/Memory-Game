33bd92b805891371af2545994be9cf90a7c5e930
Negar Kalbasi <neka@Negars-MacBook-Pro.local>
2015-10-28 12:56:52 -0400

    level selector added. bugs fixed

--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ Memory Game User Stories
 4. User should be able to try matching cards again
 5. User should be able to collect paired cards/eliminate them from the set
 6. User should be able to reset the game
+
+Explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+My approach was to create a game logic that's as independent from the user interface as possible. I started off the mvp with an array of paired integers that got shuffled and the user could guess the position of matching pairs within the array. Once I had that working, I tackled the user interface and the click events. I found that part to be the most challenging because it created many possible user errors.
+
+For the CSS I started playing with flex boxes and in talking to John and reading online I learned a lot about them but didn't implement all that I learned here. I would have liked to break down the playGame function into smaller, better defined functions based on whether they did game logic or user interface but that seemed a little daunting after I had everything working! 
--- script.js
+++ script.js
@@ -1,8 +1,97 @@
 $(document).ready(function(){
-  var deck = [];
-  var pair = [];
+  var deck = []; //array of integers that correspond to "cards"
+  var pair = []; //array that holds selected card for comparison
+
+  //This function checks attempts and matches to determine if user won or lost
+  var winOrLose = function(attempts, matches){
+    if (attempts == 0 && matches < (deck.length)/2){
+      $('.card').each(function(i){
+              var cName = 'c'+deck[i];
+              $(this).addClass(cName);
+              $('.container').append('<h1 class="prompt">Try Again!</h1>')
+      });
+    }else if (matches == (deck.length)/2 && attempts >= 0) {
+      $('.container').append('<h1 class="prompt">Well Done!</h1>');
+    }
+  }
+
+  //This function creates click events, compares pairs and counts attempts and matches
+  var playGame = function(attempts){
+    // var attempts = 20;
+    var matches = 0;
+    $('#matches').html('Matches: '+matches);
+    $('#attempts').html('Chances: '+attempts);
+    var ready = true; //game state
+      $('.card').each(function(i){
+          $(this).click(function(){
+              if(ready === true){
+                  var cName = 'c'+deck[i];
+                  $(this).addClass(cName);
+                  if(i !== pair[0]){  //prevents double clicks counting as a turn
+                    pair.push(i);
+                  }
+              }
+
+              if(pair.length === 2){
+                ready = false;
+                  var x = pair[0];
+                  var y = pair[1];
+                  if(deck[x] !== deck[y]){
+                    attempts--;
+                      setTimeout(function(){
+                        $('.card').eq(x).attr('class', 'card');
+                        $('.card').eq(y).attr('class', 'card');
+                        ready = true;
+                      }, 1500);
+                  }else{
+                    //checks to make sure the same card hasn't been clicked twice
+                    if(x !== y){
+                        matches ++;
+                        attempts ++;
+                        $('.card').eq(x).off('click');
+                        $('.card').eq(y).off('click');
+                    }
+                    ready = true;
+                  }
+                  winOrLose(attempts, matches);
+                  $('#matches').html('Matches: '+matches);
+                  $('#attempts').html('Chances: '+attempts);
+                  pair.length = 0;
+              }
+
+          });
+      });
+  }
+  //This function creates the card divs in the class container
+  var displayCards = function(){
+        for(var i=0; i<deck.length; i++){
+        $('.container').append('<div class="card"></div>');
+        };
+        if(deck.length == 8){
+          $('.card').height(250).width(250);
+          playGame(6);
+        }else if(deck.length == 18){
+          $('.card').height(175).width(175);
+          playGame(10);
+        }else{
+          playGame(20);
+        }
+  }
+  // This function uses Fisher-Yates method to shuffle the deck
+    var shuffle = function(){
+      var temp, i, n = deck.length;
+
+      while(n){
+        i = Math.floor(Math.random()*n--);
+        temp = deck[n];
+        deck[n] = deck[i];
+        deck[i] = temp;
+      }
+      displayCards();
+    }
   // This function generates random numbers from 0-20 (21 possible card patterns) and inserts them into the array twice
     var makeDeck = function(numPairs){
+      deck = [];
         for (var i=0; i<numPairs; i++){
           var rndm = Math.floor(Math.random()*20);
           if (deck.indexOf(rndm) == -1){
@@ -14,91 +103,40 @@ $(document).ready(function(){
         }
         shuffle();
     }
-  // This function uses Fisher-Yates method to shuffle the deck
-    var shuffle = function(){
-      var temp, i, n = deck.length;
 
-      while(n){
-        i = Math.floor(Math.random()*n--);
-        temp = deck[n];
-        deck[n] = deck[i];
-        deck[i] = temp;
-      }
-    }
-    //This function creates the card divs in the class container
-    var displayCards = function(){
-          for(var i=0; i<deck.length; i++){
-          $('.container').append('<div class="card"></div>');
-          };
+    //clears screen from all the dynamically added elements
+    var clearScreen = function(){
+      $('.prompt').remove();
+      $('.card').remove();
     }
 
-    var winOrLose = function(attempts, matches){
-      if (attempts == 0 && matches < 16 ){
-        $('.card').each(function(i){
-                var cName = 'c'+deck[i];
-                $(this).addClass(cName);
-                $('.container').append('<h1 class="prompt">Try Again!</h1>')
-        });
-      }else if (matches == 16 && attempts >= 0) {
-        $('.container').append('<h1 class="prompt">Well Done!</h1>');
-      }
-    }
-  //This function creates click events, compares pairs and counts attempts and matches
-    var playGame = function(){
-      var attempts = 20;
-      var matches = 0;
-      var ready = true;
-        $('.card').each(function(i){
-            $(this).click(function(){
-                if(ready === true){
-                    pair.push(i);
-                    var cName = 'c'+deck[i];
-                    $(this).addClass(cName);
-                }
-                if(pair.length === 2){
-                  ready = false;
-                    var x = pair[0];
-                    var y = pair[1];
-                    if(deck[x] !== deck[y]){
-                      attempts--;
-                        setTimeout(function(){
-                          $('.card').eq(x).attr('class', 'card');
-                          $('.card').eq(y).attr('class', 'card');
-                          ready = true;
-                        }, 1500);
-                    }else{
-                      //checks to make sure the same card hasn't been clicked twice
-                      if(x !== y){
-                          matches ++;
-                          attempts ++;
-                          $('.card').eq(x).off('click');
-                          $('.card').eq(y).off('click');
-                      }
-                      ready = true;
-                    }
-                    winOrLose(attempts, matches);
-                    $('#matches').html('Matches: '+matches);
-                    $('#attempts').html('Remaining Attempts: '+attempts);
-                    pair.length = 0;
-                }
+    //game process and levels
+    var gameStart = function (){
+    $('body').append('<h1 class="prompt">Choose a Level<h1>');
+    $('#easy').click(function(e){
+        e.preventDefault();
+        clearScreen();
+        makeDeck(4);
+    })
 
-            });
-        });
-    }
-    //function calls
-    makeDeck(16);
-    displayCards();
-    playGame();
-    //resets and calls functions again
-    $('#reset').click(function(){
-        deck = [];
-        $('#attempts').html('Remaining Attempts: 20');
-        $('#matches').html('Matches: 0');
-        $('.card').remove();
-        $('.prompt').remove();
+    $('#mid').click(function(e){
+        e.preventDefault();
+        clearScreen();
+        makeDeck(9);
+    })
+    $('#hard').click(function(e){
+        e.preventDefault();
+        clearScreen();
         makeDeck(16);
-        displayCards();
-        playGame();
+    })
+    //resets and calls functions again
+    $('#reset').click(function(e){
+        e.preventDefault();
+        gameStart();
     });
+  }
+
+    //function call
+    gameStart();
 
 });
